d7335abca6c7a0d079ed86a6ceb3d0bea9b68fef
Rizki Pandiwa <rizki.pandiwa@erajaya.com>
2024-05-16 01:17:59 +0700

    fix config db again

--- config/db.js
+++ config/db.js
@@ -2,15 +2,11 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   driver: "mysql",
-  host: "monorail.proxy.rlwy.net",
-  user: "root",
-  password: "MECvOhMsbEXhauHuSPNJvMThRpUJovVl",
-  database: "railway",
-  port: 12552,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
-console.log(pool);
 
 module.exports = pool.promise();
--- config/migrate.js
+++ config/migrate.js
@@ -5,11 +5,11 @@ const dbmigrate = DbMigrate.getInstance(true, {
   config: {
     dev: {
       driver: "mysql",
-      host: "monorail.proxy.rlwy.net",
-      user: "root",
-      password: "MECvOhMsbEXhauHuSPNJvMThRpUJovVl",
-      database: "railway",
-      port: 12552,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
     },
   },
 });
