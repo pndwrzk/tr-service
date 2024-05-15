7c73ccc3da8b1a9f8b2ba4473e8ecbf4d7e095f5
Rizki Pandiwa <rizki.pandiwa@erajaya.com>
2024-05-16 01:28:20 +0700

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const bodyParser = require("body-parser");
 
 const routes = require("./routes/router");
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
